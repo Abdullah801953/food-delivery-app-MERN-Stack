187aa6fe2e73ae64842fb1dca0b63259ebf65fc2
Abdullah801953 <132187695+Abdullah801953@users.noreply.github.com>
2025-06-02 23:23:19 +0530

    food app final commit3

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-app-mern-stack-tc4h.onrender.com";
   const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
